2299f55af6f280b5a56fd29dc7fdec1ccc0255a2
jerem509 <jcharles3@byupathway.edu>
2025-06-19 14:29:49 -0400

    website project built

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -23,3 +23,18 @@ const today = new Date();
 
 currentYear.innerHTML = `${today.getFullYear()}`;
 lastYearModified.innerHTML = `Last modified on : <span id="ddate">${new Date(document.lastModified)}</span>`;
+
+const sections = Array.from(document.querySelectorAll('.main-elt-container > section'));
+sections.forEach(section => {
+    let idx = sections.indexOf(section);
+    if (idx % 2 === 0) {
+        section.classList.add('even');
+    }
+    else
+    {
+        section.classList.add('odd');
+    }
+});
+
+const homeBtn = document.querySelector('#home-button');
+homeBtn.addEventListener('click', () => window.location.href = 'contact.html');
